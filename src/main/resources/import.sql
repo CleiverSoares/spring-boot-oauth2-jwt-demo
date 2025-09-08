63711a38d26dc67de015721529374ce000ff7a35
CleiverSoares <cleiversoares2@gmail.com>
2025-09-08 06:43:40 -0300

    Implement user authentication with BCrypt password encoding and role management

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$MAn750F1fN5lDiIU2arSpO/1oV6Y6pW32piAQp/fEH95.K5RSkHCa');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$MAn750F1fN5lDiIU2arSpO/1oV6Y6pW32piAQp/fEH95.K5RSkHCa');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
